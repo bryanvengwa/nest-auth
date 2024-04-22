d452dde60a329cfcd3acd72fa39ee532c1bc7121
bryanvengwa <bmvengwa@gmail.com>
2024-04-22 19:52:23 +0200

    create udpate user dto

--- src/users/dto/update-user.dto.ts
+++ src/users/dto/update-user.dto.ts
@@ -1,4 +1,21 @@
 import { PartialType } from '@nestjs/mapped-types';
 import { CreateUserDto } from './create-user.dto';
+import { IsOptional, IsString } from 'class-validator';
 
-export class UpdateUserDto extends PartialType(CreateUserDto) {}
+export class UpdateUserDto extends PartialType(CreateUserDto) {
+    @IsString()
+    @IsOptional()
+    firstName: string;
+  
+    @IsString()
+    @IsOptional()
+    lastName: string;
+  
+    @IsString()
+    @IsOptional()
+    password: string;
+  
+    @IsString()
+    @IsOptional()
+    userName: string;
+}
