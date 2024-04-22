375f038d873419f8e7a008594e5d1aebd594e753
bryanvengwa <bmvengwa@gmail.com>
2024-04-22 19:43:27 +0200

    created user entity

--- src/users/entities/user.entity.ts
+++ src/users/entities/user.entity.ts
@@ -1,1 +1,19 @@
-export class User {}
+import { Column, Entity } from 'typeorm';
+
+@Entity()
+export class User {
+  @Column()
+  firstName: string;
+
+  @Column()
+  lastName: string;
+
+  @Column({ unique: true })
+  userName: string;
+
+  @Column()
+  password: string;
+
+  @Column({nullable: true})
+  refreshToken: string;
+}
