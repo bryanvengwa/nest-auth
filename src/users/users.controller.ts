1a3c02218d8f4e03b0b71084a82365dbc2b0a7f6
bryanvengwa <bmvengwa@gmail.com>
2024-04-23 14:01:17 +0200

    added auth strategies

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -9,12 +9,13 @@ import {
   ParseIntPipe,
   HttpStatus,
   Query,
+  DefaultValuePipe,
 } from '@nestjs/common';
 import { UsersService } from './users.service';
 
 import { UpdateUserDto } from './dto/update-user.dto';
 import { CreateUserDto } from './dto/create-user.dto';
-import { Pagination } from 'nestjs-typeorm-paginate';
+import { IPaginationOptions, Pagination } from 'nestjs-typeorm-paginate';
 import { User } from './entities/user.entity';
 
 @Controller('users')
@@ -22,30 +23,54 @@ export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body() createUserDto : CreateUserDto ) {
-    console.log("post function running")
+  async create(@Body() createUserDto: CreateUserDto) {
+    console.log('post function running');
     return await this.usersService.create(createUserDto);
   }
 
   @Get()
   async findAll(
-    @Query() options: any,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page: number = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit: number = 1,
   ): Promise<Pagination<User>> {
-    return this.usersService.findAll(options);
- }
+    const options: IPaginationOptions = {
+      limit,
+      page,
+    };
+    return this.usersService.paginate(options);
+  }
 
   @Get(':id')
-  findOne(@Param('id', new ParseIntPipe({errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) id: number) {
+  findOne(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+  ) {
     return this.usersService.findOne(id);
   }
 
   @Patch(':id')
-  update(@Param('id', new ParseIntPipe({errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE})) id: number, @Body() updateUserDto: UpdateUserDto) {
+  update(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_ACCEPTABLE }),
+    )
+    id: number,
+    @Body() updateUserDto: UpdateUserDto,
+  ) {
     return this.usersService.update(id, updateUserDto);
   }
 
   @Delete(':id')
-  remove(@Param('id', new ParseIntPipe({errorHttpStatusCode : HttpStatus.NOT_FOUND})) id: number) {
+  remove(
+    @Param(
+      'id',
+      new ParseIntPipe({ errorHttpStatusCode: HttpStatus.NOT_FOUND }),
+    )
+    id: number,
+  ) {
     return this.usersService.remove(id);
   }
 }
