40cc99530720e6eabbb10ce6a466343f1dba1a7a
bryanvengwa <bmvengwa@gmail.com>
2024-04-24 11:29:38 +0200

    refactored strategies to use app constants

--- src/auth/strategies/refreshToken.strategy.ts
+++ src/auth/strategies/refreshToken.strategy.ts
@@ -2,6 +2,7 @@ import { PassportStrategy } from '@nestjs/passport';
 import { ExtractJwt, Strategy } from 'passport-jwt';
 import { Request } from 'express';
 import { Injectable } from '@nestjs/common';
+import { appConstants } from 'src/common/constants/app.constants';
 
 @Injectable()
 export class RefreshTokenStrategy extends PassportStrategy(
@@ -11,7 +12,7 @@ export class RefreshTokenStrategy extends PassportStrategy(
   constructor() {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
-      secretOrKey: process.env.JWT_REFRESH_SECRET || '4B+H+Np+J7Ia1VqXbApbbnqbNnO7PSzRRLvzRFndaapRvHaVHTz1R257eNhEPSghQQvJWhBmthjjba0hSt8zlpYH3MT2OhRcZPa0UOVVUum7udoaSWrO0KpCMzjkOx5yemojSf1i9I2x9fYU/WsI23MtBUMtezf+bJ8D0NMCU5iQEpg4wWQGpwR6VU0SkiVgNcKJD9Q6YgJTOMlyu1zkT63xtYXvotZye0LsbukDcAf+kj/HS99KPkZ3YXFp8mgM40ETTH37C4UiAU6X8c+ZOhKgdbHTeF5EoePL8uKnQKKy3lhHli0OJBJKDIWd8Rn7pwd/6n5+E6kR',
+      secretOrKey: appConstants.secret,
       passReqToCallback: true,
     });
   }
