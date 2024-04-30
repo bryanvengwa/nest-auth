be570f306d9ffd2526a08c050593a41a690b72d2
bryanvengwa <bmvengwa@gmail.com>
2024-04-30 19:12:12 +0200

    added admins table

--- src/admin/entities/admin.entity.ts
+++ src/admin/entities/admin.entity.ts
@@ -1,1 +1,12 @@
-export class Admin {}
+import { User } from 'src/users/entities/user.entity';
+import { Entity, JoinColumn, OneToOne, PrimaryGeneratedColumn } from 'typeorm';
+
+@Entity('admins')
+export class Admin {
+  @PrimaryGeneratedColumn()
+  id: number;
+  
+  @OneToOne(() => User)
+  @JoinColumn()
+  user: User;
+}
